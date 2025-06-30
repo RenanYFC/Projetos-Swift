41cc71501952d8fdca00898c05a008492b3821f7
Renan Yamamoto Faria do Carmo <101796281+RenanYFC@users.noreply.github.com>
2025-06-30 20:30:06 +0000

    Projeto Final

--- Sources/27julho-Exercicios-Optionals/to-do-list.swift
+++ Sources/27julho-Exercicios-Optionals/to-do-list.swift
@@ -2,21 +2,35 @@
 
 Enunciado:
 
-Crie um programa que simula um sistema simples de lista de tarefas (To-Do List) no terminal.
-Ele terá 5 funções, no momento vamos construir três:
-Mostrar menu de opções
-Adicionar tarefa
-Remover Tarefa
+Criar um app de tarefas no terminal para praticar os conceitos aprendidos no curso. 
+O sistema deve permitir adicionar, listar, editar e remover tarefas, tudo de forma 
+interativa.
+
+Funcionalidades necessárias:
+• listarTarefas() – Mostra todas as tarefas com numeração.
+• adicionarTarefa() – Lê uma descrição do usuário e adiciona à lista.
+• removerTarefa() – Pede o número da tarefa e a remove.
+• editarTarefa() – Permite alterar o texto de uma tarefa.
+• encerrarPrograma() – Encerra o app quando o usuário digita 0.
+Obs: Implemente as funções abaixo, cada uma com uma única responsabilidade
 
 */
 
 import Foundation
+
+var tarefasGlobal = [String]()
+
+var parar = false
+
 func exibirMenu() {
     print ("""
     ===== To-Do List =====
     
-    Digite 1 para adicionar tarefas
-    Digite 2 para remover tarefas
+    Digite 1 para exibir as tarefas
+    Digite 2 para adicionar tarefas
+    Digite 3 para remover tarefas
+    Digite 4 para editar uma tarefa
+    Digite 0 para encerrar o programa
     
     """
     )
@@ -26,10 +40,89 @@ func exibirMenu() {
     if let opcEscolhida = opcEscolhida {
         if let opcEscolhidaInt = Int(opcEscolhida) {
             switch opcEscolhidaInt {
+                case 0:
+                    parar = true
                 case 1:
-                    adicionarTarefa()
+                    if tarefasGlobal.count > 0 {
+                        print("=== Suas Tarefas ===")
+                        exibirTarefas(tarefas: tarefasGlobal)
+                    
+                    }
+                    else {
+                        print ("Você não possui tarefas!")
+                    }
                 case 2:
-                    removerTarefa()
+                    
+                    print("Descreva sua tarefa: ",terminator:"")
+                    let descricaoTarefa = readLine()
+                    
+                    if let descricaoTarefa = descricaoTarefa {
+                        if (!descricaoTarefa.isEmpty) {
+                            print("Adicionado com sucesso!\n")
+                            adicionarTarefa(descricaoTarefa: descricaoTarefa, tarefas: tarefasGlobal)
+                        }
+                        else {
+                            print("Insira um valor válido!\n")
+                        }
+                    
+                    }
+                    else {
+                        print("\nNenhum valor recebido, encerrando o programa...")
+                    }
+                    
+                case 3:
+                print ("Digite o índice da tarefa que você quer remover: ",terminator: "")
+                let indiceTarefa = readLine()
+                
+                if let indiceTarefa = indiceTarefa {
+                    if let indiceTarefaInt = Int(indiceTarefa) {
+                        
+                        if(indiceTarefaInt <= tarefasGlobal.count-1 && indiceTarefaInt >= 0) {
+                            removerTarefa(indiceTarefa: indiceTarefaInt, tarefas: tarefasGlobal)
+                        }
+                        else {
+                            print("Essa tarefa não existe\n")
+                        }
+                    }
+                    else {
+                        print("Digite somente números inteiros!\n")
+                    }
+                }
+                else {
+                    print("\nNenhum valor recebido, encerrando o programa...")
+                }
+                case 4:
+                    print("Digite o índice da tarefa que você quer editar: ",terminator:"")
+                    let indiceTarefa = readLine()
+                    
+                    if let indiceTarefa = indiceTarefa {
+                        if let indiceTarefaInt = Int(indiceTarefa) {
+                            if (indiceTarefaInt < tarefasGlobal.count) {
+                            
+                                print("Descreva sua tarefa: ",terminator:"")
+                                let descricaoTarefa = readLine()
+                                
+                                if let descricaoTarefa = descricaoTarefa {
+                                    if (!descricaoTarefa.isEmpty) {
+                                        editarTarefa(indiceTarefa: indiceTarefaInt, descricaoTarefa: descricaoTarefa, tarefas: tarefasGlobal)
+                                    }
+                                    else {
+                                        print("Insira um valor válido!\n")
+                                    }                
+                                }
+                            
+                            }
+                            else {
+                                print ("Digite um valor dentro do índice!")
+                            }
+                        }
+                        else {
+                            print ("Digite um número válido!")
+                        }
+                    }
+                    else {
+                        print("\nNenhum valor recebido, encerrando o programa...")
+                    }
                 default:
                     print("Valor não correspondente!")
                     exibirMenu()
@@ -37,62 +130,52 @@ func exibirMenu() {
         }
         else {
             print("Insira um valor válido!\n")
-            exibirMenu()
         }
     }
     else {
         print("\nNenhum valor recebido, encerrando o programa...")
-        return
     }
-}
-var tarefas = [String]()
-
-func adicionarTarefa() {
-    print("Descreva sua tarefa: ",terminator:"")
-    let descricaoTarefa = readLine()
-    
-    if let descricaoTarefa = descricaoTarefa {
-        if (!descricaoTarefa.isEmpty) {
-            print("Adicionado com sucesso!\n")
-            tarefas.append(descricaoTarefa)
-            exibirMenu()
-        }
-        else {
-            print("Insira um valor válido!\n")
-            exibirMenu()
-        }
-    
-    }
-    else {
-        print("\nNenhum valor recebido, encerrando o programa...")
-        return
-    }
+    return
 }
 
-func removerTarefa() {
-    print ("Digite o índice da tarefa que você quer remover: ",terminator: "")
-    let indiceTarefa = readLine()
-    
-    if let indiceTarefa = indiceTarefa {
-        if let indiceTarefaInt = Int(indiceTarefa) {
-            
-            if(indiceTarefaInt <= tarefas.count-1 && indiceTarefaInt >= 0) {
-                tarefas.remove(at:indiceTarefaInt)
-                print("Remoção feita com sucesso!\n")
-                exibirMenu()
-            }
-            else {
-                print("Essa tarefa não existe\n")
-                exibirMenu()
-            }
-        }
-        else {
-            print("Digite somente números inteiros!\n")
-            exibirMenu()
-        }
-    }
-    else {
-        print("\nNenhum valor recebido, encerrando o programa...")
-        return
-    }
+func adicionarTarefa(descricaoTarefa: String, tarefas: [String]) {
+    var listaTarefasLocal = tarefas
+    listaTarefasLocal.append(descricaoTarefa)
+    return tarefasGlobal = listaTarefasLocal
 }
+
+func removerTarefa(indiceTarefa: Int, tarefas: [String]) {
+
+    var listaTarefasLocal = tarefas
+    listaTarefasLocal.remove(at:indiceTarefa)
+    print("Remoção feita com sucesso!\n")
+    return tarefasGlobal = listaTarefasLocal
+    
+}
+
+func exibirTarefas(tarefas: [String]) {
+    
+    for i in 0..<tarefas.count {
+            print("índice[\(i)]: \(tarefas[i]) \n")
+        }
+    
+    print("=============")
+    
+    return
+}
+
+func editarTarefa(indiceTarefa: Int, descricaoTarefa: String, tarefas: [String]) {
+
+    var listaTarefasLocal = tarefas
+    
+    listaTarefasLocal[indiceTarefa] = descricaoTarefa
+    print("Alterado com sucesso!\n")
+    
+    return tarefasGlobal = listaTarefasLocal
+
+}
+/**
+while parar == false {
+    exibirMenu()    
+}
+*/
